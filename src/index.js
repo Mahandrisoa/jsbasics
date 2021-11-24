154300a6ce4ce240afdfc199ab0d4255c44d3006
Mahandrisoa ANDRIAMAHERISOA <andriamaherisoamahandrisoa@gmail.com>
2021-11-24 06:22:36 +0200

    Clone repository from private server

---
+++ src/index.js
@@ -0,0 +1,218 @@
+/**
+ * Return true if it's even, and false if it isn't.
+ *
+ * @param {Number} i
+ */
+export const isNumberEven = i => {
+  return Math.abs(i % 2) === 0;
+};
+
+/**
+ * `str` is a string, but it may not have a file extension.
+ * Return the file extension (with no period) if it has one, otherwise false
+ * @param {String} str
+ */
+export const getFileExtension = str => {
+  let result = str.match(/\w\.(\w+)$/);
+  return result ? result[1] : false;
+};
+
+/**
+ * `arr`is a string.
+ * Return the longest string in the array
+ *
+ * @param {String} arr
+ */
+export const longestString = arr => {
+  return arr
+    .sort((a, b) =>
+      typeof a === 'string' && typeof b === 'string' && a.length < b.length
+        ? 1
+        : -1
+    )
+    .shift();
+};
+
+/**
+ * `str` is an string.
+ * Return a new string who's characters are in the opposite order to str's.
+ * @param {String} str
+ */
+export const reverseString = str => {
+  return str
+    .split('')
+    .reverse()
+    .join('');
+};
+
+/**
+ * `str` is a string.
+ * Return true if it is a palindrome and false otherwise.
+ * It should be case insensitive and not consider space or punctuation.
+ *
+ * @param {String} str
+ */
+export const isPalindrome = str => {
+  let splittedString = str.split('');
+  let firstWord = splittedString.shift();
+  let lastWord = splittedString.pop();
+  return firstWord.toUpperCase() === reverseString(lastWord).toUpperCase();
+};
+
+/**
+ * `arr` will be an array containing integers, strings and/or arrays like itself.
+ * Return the sum all the numbers you find, anywhere in the nest of arrays.
+ */
+export const nestedSum = arr => {
+  const flatDeep = toFlat => {
+    return toFlat.reduce(
+      (acc, val) =>
+        acc.concat(
+          Array.isArray(val) ? flatDeep(val) : typeof val === 'number' ? val : 0
+        ),
+      []
+    );
+  };
+  return flatDeep(arr).reduce((a, b) => a + b);
+};
+
+/**
+ * Retire du tableau `tab` passé en paramètre les éléments passés en
+ * paramètres dans `elms`.
+ *
+ * On utilise la destructuration pour pouvoir utiliser tous les arguments
+ * après `tab` comme un tableau.
+ *
+ * Après l'exécution le tableau d'origine est réellement modifié, ce
+ * on ne retourne pas une copie.
+ *
+ * Exemple :
+ * let tab = ['a', 'b', 'c', 'b', 'c'];
+ * pull(tab, 'a', 'b');
+ * tab; // ['c']
+ *
+ * @param {Array} tab
+ * @param  {objects} elms
+ */
+export const retireDe = (tab, ...elms) => {
+  let setElms = new Set(elms);
+  setElms.forEach(val => {
+    let idx = tab.indexOf(val);
+    while (idx != -1) {
+      tab.splice(idx, 1);
+      idx = tab.indexOf(val, idx + 1);
+    }
+  });
+  return tab;
+};
+
+/**
+ * Aplatit en profondeur un tableau passé en paramètre.
+ *
+ * Indications :
+ * - Utiliser la récursion.
+ * - Utiliser `Array.prototype.concat()` avec un tableau vide ([]) et l'opérateur de déstructuration (...) pour aplatir  un tableau.
+ *
+ * Exemple :
+ * aplatirRecursif([5, [4], [[3], 2], [1], 0]);
+ * // [5, 4, 3, 2, 1, 0]
+ */
+// export const aplatirRecursif = tab => {
+//   const flatDeep = toFlat => {
+//     return toFlat.reduce(
+//       (acc, val, index) =>
+//         Array.isArray(val)
+//           ? acc.concat(flatDeep(val.filter(x => x !== null)))
+//           : [...acc, val],
+//       []
+//     );
+//   };
+//   return flatDeep(tab);
+// };
+
+export const aplatirRecursif = tab => {
+  var result = [];
+  for (var i = 0; i < tab.length; i++) {
+    if (Array.isArray(tab[i])) {
+      result = result.concat(aplatirRecursif(tab[i]));
+    } else {
+      result.push(tab[i]);
+    }
+  }
+  return result;
+};
+
+/**
+ * Retourne la liste de toutes les permutations des objets du tableau passé en paramètre.
+ *
+ * Exemple :
+ * permutations([0,1,2]);
+ * // [ [ 0, 1, 2 ],
+ * //   [ 0, 2, 1 ],
+ * //   [ 1, 0, 2 ],
+ * //   [ 1, 2, 0 ],
+ * //   [ 2, 0, 1 ],
+ * //   [ 2, 1, 0 ] ]
+ *
+ * @param {Array} tab
+ */
+export const permutations = tab => {
+  if (tab.length <= 2) {
+    return tab.length === 2 ? [tab, [tab[1], tab[0]]] : tab;
+  }
+  return tab.reduce(
+    (acc, item, i) =>
+      acc.concat(
+        permutations([...tab.slice(0, i), ...tab.slice(i + 1)]).map(val => [
+          item,
+          ...val,
+        ])
+      ),
+    []
+  );
+};
+
+/**
+ * Retourne un élément au hazard parmi les éléments du tableau `tab` passé en
+ * paramètre.
+ *
+ * @param {Array} tab
+ */
+export const echantillon = tab => tab[Math.floor(Math.random() * tab.length)];
+
+/**
+ * Prend un tableau 'tab' et le transforme en string avec chaque élément séparé par le `separateur`.
+ * Les deux derniers éléments sont séparé pas le séparateur `fin`.
+ *
+ * Exemple:
+ * enumerer(['Riri', 'Fifi', 'Loulou'], ', ', ' et ');
+ * // 'Riri, Fifi et Loulou'
+ *
+ *
+ * @param {Array} tab
+ * @param {string} separateur
+ * @param {string} fin
+ */
+export const enumerer = (tab, separateur = ', ', fin = separateur) => {
+  return tab.join(separateur).replace(/, ([^,]*)$/, ` ${fin} $1`);
+};
+
+/**
+ * Retourne, sous forme d'un tableau, les `n` plus grand nombres du tableau `tab` passé en paramètre.
+ *
+ * Attention, on ne doit pas modifier le tableau d'origine.
+ *
+ * Utiliser `Array.prototype.sort()`, l'opérateur de destructuration (...) et `Array.prototype.slice()`
+ */
+export const nMax = (tab, n = 1) => {
+  const numbers = tab
+    .reduce((acc, val) => (typeof val === 'number' ? [...acc, val] : []), [])
+    .sort((a, b) => b - a); // descending sort
+
+  if (n < tab.length) {
+    return numbers.splice(0, n);
+  } else {
+    // if n > tab.length or equal to 0
+    return numbers;
+  }
+};
---
+++ src/index.test.js
@@ -0,0 +1,373 @@
+import {
+  isNumberEven,
+  getFileExtension,
+  longestString,
+  reverseString,
+  isPalindrome,
+  nestedSum,
+  retireDe,
+  aplatirRecursif,
+  permutations,
+  echantillon,
+  nMax,
+  enumerer,
+} from '../src';
+
+describe('JS Basics Tests', () => {
+  describe('Test isNumberEven', () => {
+    test('42 should be even', () => {
+      expect(isNumberEven(42)).toBe(true);
+    });
+
+    test('-1 should be odd', () => {
+      expect(isNumberEven(-1)).toBe(false);
+    });
+
+    test('33 should be odd', () => {
+      expect(isNumberEven(33)).toBe(false);
+    });
+
+    test('0 should be even', () => {
+      expect(isNumberEven(0)).toBe(true);
+    });
+
+    const rnd = Math.floor(Math.random() * 1000000) * 2;
+    test('random even number should be even', () => {
+      expect(isNumberEven(rnd)).toBeTruthy();
+    });
+  });
+
+  describe('Test getFileExtension', () => {
+    test('Should be "png"', () => {
+      expect(getFileExtension('image.png')).toEqual('png');
+    });
+
+    test('Should be "torrent"', () => {
+      expect(getFileExtension('perfectlylegal.torrent')).toEqual('torrent');
+    });
+
+    test('Spaces are ok in file names', () => {
+      expect(getFileExtension('Spaces are ok in file names.txt')).toEqual(
+        'txt'
+      );
+    });
+
+    test('last extension token only', () => {
+      expect(getFileExtension('archive.tar.gz')).toEqual('gz');
+    });
+
+    test('Should return false when no extension is found', () => {
+      expect(getFileExtension('no extension here')).toBe(false);
+    });
+  });
+
+  describe('Test longestString', () => {
+    test('The longest string is the last one: "abc"', () => {
+      expect(longestString(['a', 'ab', 'abc'])).toEqual('abc');
+    });
+
+    test("be sure it's a string", () => {
+      expect(longestString(['big', [0, 1, 2, 3, 4], 'tiny'])).toEqual('tiny');
+    });
+
+    test('utf-8...', () => {
+      expect(longestString(['Holà', '\u265E', '你好'])).toEqual('Holà');
+    });
+
+    test('various types', () => {
+      expect(longestString([true, false, 'lol'])).toEqual('lol');
+    });
+
+    test('various types again', () => {
+      expect(
+        longestString([{ object: true, mainly: 'to confuse you' }, 'x'])
+      ).toEqual('x');
+    });
+  });
+
+  describe('Test reverseString', () => {
+    test('Should reverse normal chars in a string', () => {
+      expect(reverseString('Hello World!')).toEqual('!dlroW olleH');
+    });
+
+    test('Should reverse spaces too', () => {
+      expect(reverseString('   abc')).toEqual('cba   ');
+    });
+  });
+
+  describe('Test isPalindrome', () => {
+    test('Should check ascii palindrome with spaces', () => {
+      expect(isPalindrome('rats live on no evil star')).toBeTruthy();
+    });
+    test('Should check ascii palindrome with upper cases', () => {
+      expect(isPalindrome('Able was I ere I saw Elba')).toBeTruthy();
+    });
+    test('Should check utf8', () => {
+      expect(isPalindrome('たけやぶやけた')).toBeTruthy();
+    });
+    test('nope', () => {
+      expect(isPalindrome('not this one...')).toBe(false);
+    });
+  });
+
+  describe('Test nestedSum', () => {
+    test('Should work with flat number array', () => {
+      expect(nestedSum([1, 2, 3, 4, 5])).toBe(15);
+    });
+
+    test('Should work with nested array and other types', () => {
+      expect(nestedSum([[1, 2, false], '4', '5'])).toBe(3);
+    });
+
+    test('Should work with deeply nested arrays', () => {
+      expect(nestedSum([[[[[[[[[1]]]]]]]], 1])).toBe(2);
+    });
+
+    test('Should work with deeply nested array + Strings', () => {
+      expect(nestedSum([['A', 'B', 'C', 1, 2, 3]])).toBe(6);
+    });
+
+    test('Should be careful not to coerce values', () => {
+      expect(nestedSum(['1', '2', '3', 1, 2, 3])).toBe(6);
+    });
+
+    test('Should return 0 is no number found', () => {
+      expect(nestedSum(['nope', false, 'nada', ['rien']])).toBe(0);
+    });
+  });
+
+  describe('Test retireDe', () => {
+    test('Retirer plusieurs elements identiques', () => {
+      let myArray = ['a', 'b', 'c', 'a', 'd', 'c', 'z'];
+      const expected = ['b', 'd', 'z'];
+      retireDe(myArray, 'a', 'c');
+      expect(myArray).toEqual(expected);
+    });
+
+    test('Retirer tous les éléments', () => {
+      let myArray = ['a', 'b', 'c', 'a', 'd', 'c'];
+      const expected = [];
+      retireDe(myArray, 'a', 'b', 'c', 'd');
+      expect(myArray).toEqual(expected);
+    });
+
+    test('Doublons à retirer', () => {
+      let myArray = ['alpha', 'beta', 'gamma'];
+      const expected = ['gamma'];
+      retireDe(myArray, 'alpha', 'beta', 'alpha', 'beta');
+      expect(myArray).toEqual(expected);
+    });
+
+    test('Ne rien retirer', () => {
+      let myArray = ['alpha', 'beta', 'gamma'];
+      const expected = ['alpha', 'beta', 'gamma'];
+      retireDe(myArray, 'nope', 'truc');
+      expect(myArray).toEqual(expected);
+    });
+  });
+
+  describe('Test echantillon', () => {
+    const monTab = [5, 4, 3, 2, 1, 0];
+    const result = echantillon(monTab);
+
+    test('Should return an defined element', () => {
+      expect(monTab.includes(result)).toBeDefined();
+    });
+
+    test('Should return undefined', () => {
+      expect(echantillon([])).toBeUndefined();
+    });
+
+    test('Should return an element contained in the array', () => {
+      expect(monTab).toContain(result);
+    });
+  });
+
+  describe('Test aplatirRecursif', () => {
+    test('aplatir', () => {
+      const monTab = [5, [4], [[3], 2], [1], 0];
+      const attendu = [5, 4, 3, 2, 1, 0];
+      expect(aplatirRecursif(monTab)).toEqual(attendu);
+    });
+    test('vide', () => {
+      expect(aplatirRecursif([])).toEqual([]);
+    });
+    test('très imbriqué', () => {
+      expect(aplatirRecursif([[[[[[[[[1]]]]]]]], 1])).toEqual([1, 1]);
+    });
+    test('cases vides', () => {
+      const monTab = [1, [2], 4];
+      monTab[1][5] = 3;
+      monTab[4] = 5;
+      const attendu = [1, 2];
+      attendu[6] = 3;
+      attendu[7] = 4;
+      attendu[9] = 5;
+      expect(aplatirRecursif(monTab)).toEqual(attendu);
+    });
+  });
+
+  /**
+   * Tester la fonction `permutations`.
+   *
+   * Cette fonction retourne la liste de toutes les permutations des objets du tableau passé en paramètre.
+   *
+   * Bien penser à tous les cas de figures : tableaux avec 1, 2, ou plusieurs éléments, doublons, etc.
+   *
+   * Exemple :
+   * permutations([0,1,2]);
+   * // [ [ 0, 1, 2 ],
+   * //   [ 0, 2, 1 ],
+   * //   [ 1, 0, 2 ],
+   * //   [ 1, 2, 0 ],
+   * //   [ 2, 0, 1 ],
+   * //   [ 2, 1, 0 ] ]
+   *
+   */
+  describe('Test permutations', () => {
+    test('tableau vide', () => {
+      expect(permutations([])).toEqual([]);
+    });
+    test('tableau avec un élèment', () => {
+      expect(permutations([6]).length).toEqual(1);
+      expect(permutations([6])).toEqual([6]);
+    });
+
+    test('nombre de permutations possible', () => {
+      expect(permutations([1, 3])).toHaveLength(2);
+    });
+
+    test('tableau avec 2 élèments', () => {
+      const expected = [
+        [1, 3],
+        [3, 1],
+      ];
+      expect(permutations([1, 3])).toEqual(expect.arrayContaining(expected));
+    });
+    test('tableau avec plusieurs élèments', () => {
+      const expected = [
+        [0, 1, 2],
+        [0, 2, 1],
+        [1, 0, 2],
+        [1, 2, 0],
+        [2, 0, 1],
+        [2, 1, 0],
+      ];
+      expect(permutations([0, 1, 2])).toEqual(expect.arrayContaining(expected));
+      // if is in the same order
+      expect(permutations([0, 1, 2])).toEqual(expected);
+    });
+    test('existance de doublons', () => {
+      const expected = [
+        [0, 1, 1],
+        [0, 1, 1],
+        [1, 0, 1],
+        [1, 1, 0],
+        [1, 0, 1],
+        [1, 1, 0],
+      ];
+      expect(permutations([0, 1, 1])).toEqual(expect.arrayContaining(expected));
+    });
+  });
+
+  /**
+   * Tester la fonction `echantillon`.
+   *
+   * Cette fonction retourne un élément au hazard parmi les éléments du tableau `tab` passé en paramètre.
+   *
+   * Attention : cette fonction utilise un module externe (Math.random) qu'il faut simuler (Mock).
+   * Voir la documentation des [fonctions Mock](https://jestjs.io/docs/en/mock-functions.html) de Jest.
+   *
+   */
+  describe('Test echantillon', () => {
+    let spy;
+    const arr = [1, 2, false, []];
+    beforeEach(() => {
+      // spy on random function
+      spy = jest.spyOn(Math, 'random');
+    });
+
+    test('spied function has been invoked and result is equal', () => {
+      // la succession de ces appels est important pour prendre la valeur
+      // spied
+      const result = echantillon(arr);
+      const idx = Math.floor(spy.mock.results[0].value * arr.length);
+      expect(spy).toHaveBeenCalled();
+      expect(result).toEqual(arr[idx]);
+    });
+
+    test('echantillon sur tableau à un élèment', () => {
+      expect(echantillon([1])).toEqual(1);
+    });
+
+    test('echantillon sur tableau vide', () => {
+      expect(echantillon([])).toBeUndefined();
+    });
+  });
+
+  /**
+   * Tester la fonction `enumerer`.
+   *
+   * Cette fonction prend un tableau 'tab' et le transforme en string avec chaque élément séparé par le `separateur`.
+   * Les deux derniers éléments sont séparé pas le séparateur `fin`.
+   *
+   * Exemple:
+   * enumerer(['Riri', 'Fifi', 'Loulou'], ", ", " et ");
+   * // 'Riri, Fifi et Loulou'
+   *
+   * Attention aux cas particuliers (tableaux à 0, 1 ou 2 éléments.)
+   */
+  describe('Test enumerer', () => {
+    test('tableau vide', () => {
+      expect(enumerer([])).toEqual('');
+    });
+
+    test('tableau à 2 valeurs', () => {
+      expect(enumerer(['Tom', 'Jerry'], ', ', 'et')).toEqual('Tom et Jerry');
+    });
+
+    test('tableau à plusieurs valeurs', () => {
+      expect(enumerer(['Rouen', 'Le Havre', 'Caen'], ', ', 'et')).toEqual(
+        'Rouen, Le Havre et Caen'
+      );
+    });
+  });
+
+  /**
+   * Tester la fonction `nMax`.
+   *
+   * Cette fonction retourne, sous forme d'un tableau, les `n` plus grand nombres du tableau `tab` passé en paramètre.
+   *
+   * Attention, on ne doit pas modifier le tableau d'origine.
+   *
+   * Exemples:
+   * nMax([1, 2, 3]); // [3]
+   * nMax([1, 2, 3], 2); // [3,2]
+   * nMax([1, 2, 3], 20); // [3,2,1]
+   * nMax(['1', '2', 3], 2); // [3]
+   * nMax(['1', '2', '3'], 2); // []
+   *
+   */
+  describe('Test nMax', () => {
+    test('niveau par défaut', () => {
+      expect(nMax([1, 2, 3])).toEqual([3]);
+    });
+    test('retourne les 2-Max values', () => {
+      expect(nMax([1, 2, 3], 2)).toEqual([3, 2]);
+    });
+
+    test('retourne les n-Max values', () => {
+      expect(nMax([1, 2, 3], 20)).toEqual([3, 2, 1]);
+    });
+
+    describe('ne marche que sur les tableau de Number', () => {
+      test('plus grand nombre depuis le multi-type', () => {
+        expect(nMax(['1', '2', 3], 2)).toEqual([3]);
+      });
+
+      test("n'agit que sur les tableaux de number", () => {
+        expect(nMax(['1', '2', '3'], 2)).toEqual([]);
+      });
+    });
+  });
+});
